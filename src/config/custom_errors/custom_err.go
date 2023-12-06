399a00526f8799475a110394e82c387c0a863871
Lucas Silva <lucas.oliveirasilva@wexinc.com>
2023-12-06 19:22:35 -0300

    Custom error creation
    (cherry picked from commit 67ebc5b890c8c0aa5ddc4370e9ec9597e312505d)

---
+++ src/config/custom_errors/custom_err.go
@@ -0,0 +1,61 @@
+package custom_errors
+
+import "net/http"
+
+type CustomErr struct {
+	Message string   `json:"message,omitempty"`
+	Err     string   `json:"error,omitempty"`
+	Code    int      `json:"code,omitempty"`
+	Causes  []Causes `json:"causes,omitempty"`
+}
+
+type Causes struct {
+	Field   string `json:"field,omitempty"`
+	Message string `json:"message,omitempty"`
+}
+
+func (r *CustomErr) Error() string {
+	return r.Message
+}
+
+func NewCustomErr(message, err string, code int, causes []Causes) *CustomErr {
+	return &CustomErr{
+		Message: message,
+		Err:     err,
+		Code:    code,
+		Causes:  causes,
+	}
+}
+
+func NewBadRequestError(message string) *CustomErr {
+	return &CustomErr{
+		Message: message,
+		Err:     "bad_rest",
+		Code:    http.StatusBadRequest,
+	}
+}
+
+func NewValidationError(message string, causes []Causes) *CustomErr {
+	return &CustomErr{
+		Message: message,
+		Err:     "bad_rest",
+		Code:    http.StatusBadRequest,
+		Causes:  causes,
+	}
+}
+
+func NewInternalServerError(message string) *CustomErr {
+	return &CustomErr{
+		Message: message,
+		Err:     "internal_server_error",
+		Code:    http.StatusInternalServerError,
+	}
+}
+
+func NewNotFoundError(message string) *CustomErr {
+	return &CustomErr{
+		Message: message,
+		Err:     "not_found",
+		Code:    http.StatusNotFound,
+	}
+}
