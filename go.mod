f9b4e81cbf00b1f9ac018c0eb01b2d2759c230b5
Lucas Silva <lucas.oliveirasilva@wexinc.com>
2023-12-26 13:45:19 -0300

    Creating find user service test

--- go.mod
+++ go.mod
@@ -46,14 +46,17 @@ require (
 	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
 	go.mongodb.org/mongo-driver v1.13.1 // indirect
 	go.uber.org/atomic v1.9.0 // indirect
+	go.uber.org/mock v0.4.0 // indirect
 	go.uber.org/multierr v1.9.0 // indirect
 	golang.org/x/arch v0.6.0 // indirect
 	golang.org/x/crypto v0.16.0 // indirect
 	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
+	golang.org/x/mod v0.12.0 // indirect
 	golang.org/x/net v0.19.0 // indirect
 	golang.org/x/sync v0.5.0 // indirect
 	golang.org/x/sys v0.15.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
+	golang.org/x/tools v0.13.0 // indirect
 	google.golang.org/protobuf v1.31.0 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
---
+++ test/service/find_user_test.go
@@ -0,0 +1,49 @@
+package service
+
+import (
+	"github.com/lucasolsi-wex/go-crud/src/config/custom_errors"
+	"github.com/lucasolsi-wex/go-crud/src/model"
+	"github.com/lucasolsi-wex/go-crud/src/model/service"
+	"github.com/lucasolsi-wex/go-crud/test/mocks"
+	"github.com/stretchr/testify/assert"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.uber.org/mock/gomock"
+	"testing"
+)
+
+func TestUserDomainService_FindUserByIDServices(t *testing.T) {
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+
+	repository := mocks.NewMockUserRepository(controller)
+	userService := service.NewUserDomainService(repository)
+
+	t.Run("if_user_with_id_exists_then_returns_successfully", func(t *testing.T) {
+		id := primitive.NewObjectID().Hex()
+
+		userDomain := model.NewUserDomain("First", "Name", "email@email.com", 34)
+		userDomain.SetId(id)
+
+		repository.EXPECT().FindUserById(id).Return(userDomain, nil)
+
+		userDomainReturn, err := userService.FindUserById(id)
+		assert.Nil(t, err)
+		assert.NotNil(t, userDomainReturn)
+		assert.EqualValues(t, userDomainReturn.GetId(), id)
+		assert.EqualValues(t, userDomainReturn.GetFirstName(), userDomain.GetFirstName())
+		assert.EqualValues(t, userDomainReturn.GetLastName(), userDomain.GetLastName())
+		assert.EqualValues(t, userDomainReturn.GetEmail(), userDomain.GetEmail())
+		assert.EqualValues(t, userDomainReturn.GetAge(), userDomain.GetAge())
+	})
+
+	t.Run("if_user_does_not_exists_then_return_error", func(t *testing.T) {
+		id := primitive.NewObjectID().Hex()
+
+		repository.EXPECT().FindUserById(id).Return(nil, custom_errors.NewUserNotFoundError("User not found!"))
+
+		userDomainReturn, err := userService.FindUserById(id)
+		assert.Nil(t, userDomainReturn)
+		assert.NotNil(t, err)
+		assert.EqualValues(t, err.Message, "User not found!")
+	})
+}
